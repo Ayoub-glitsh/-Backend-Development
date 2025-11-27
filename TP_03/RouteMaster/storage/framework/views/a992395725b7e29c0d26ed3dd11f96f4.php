e32beedcfa36eb8a49d86869f7b69c9f37ffe1fb
Ayoub-glitsh <ayoubaguezzar1@gmail.com>
2025-11-27 22:06:16 +0100

    Add files via upload

---
+++ TP_03/RouteMaster/storage/framework/views/a992395725b7e29c0d26ed3dd11f96f4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is the Ayoub view.</h1>
+
+    <h2>More information about Ayoub</h2>
+
+    <ul>
+        <li>Eventually, my name is Ayoub.</li>
+    </ul>
+
+
+    <a href="/">
+        <button>back</button>
+    </a>
+
+    <p> <?php echo e($name); ?></p>
+
+</body>
+</html>
+<?php /**PATH C:\Users\HP\Desktop\Laravel-TP\Ayoub\resources\views/ayoub.blade.php ENDPATH**/ ?>
---
+++ TP_03/RouteMaster/storage/framework/views/b926e05812dd20a05dcaf5853caa3046.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to Ayoub's Application</h1>
+
+    <a href="/ayoub" class="btn">
+        Find Ayoub's View
+    </a>
+</body>
+</html>
+<?php /**PATH C:\Users\HP\Desktop\Laravel-TP\Ayoub\resources\views/welcome.blade.php ENDPATH**/ ?>
